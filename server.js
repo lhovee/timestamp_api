01873377513c9584edb1595a2d57019a642b7ee5
Luke Hovee <lhovee16@gmail.com>
2016-07-29 17:25:45 +0000

    "this project is complete"

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+var moment = require('moment');
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
+
+app.get('/:time', function(req, res) {
+    var date = req.param('time');
+    if (moment(date).isValid) {
+    res.send({natural: moment(date).format('MMMM Do YYYY'), unix: moment(date).unix()})}
+    else if (moment(date).isValid==false) {
+        res.send({natural: null, unix: null});
+    }
+}); 
 
 app.listen(8080, function() {
-    console.log('example app listening on port 8080!');
+    console.log('Let us timestamp app!');
 });
 
-/*to view Window-Share-Application-Open  */
+/*
+
+https://scotch.io/tutorials/use-expressjs-to-get-url-and-post-parameters
+
+*/
